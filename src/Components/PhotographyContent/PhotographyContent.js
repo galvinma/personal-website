e125dfaf18cbe80ecedd6c4551c3a36df6007b62
Matthew Galvin <matthew.galvin@invitae.com>
2022-02-27 10:46:24 -0800

    Upate photography content to masonry

--- src/Components/PhotographyContent/PhotographyContent.js
+++ src/Components/PhotographyContent/PhotographyContent.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ImageMasonry from 'react-image-masonry';
 
 // CSS
 import './PhotographyContent.scss'
@@ -7,13 +8,13 @@ import './PhotographyContent.scss'
 import aboveguitar from '../.././Images/Gallery/aboveguitar.jpg'
 import alpsRolling from '../.././Images/Gallery/alpsRolling.jpg'
 import arno from '../.././Images/Gallery/arno.jpg'
-import beach_camp from '../.././Images/Gallery/beach_camp.jpg'
 import ca_coast from '../.././Images/Gallery/ca_coast.jpg'
 import carlsbad_sunset from '../.././Images/Gallery/carlsbad_sunset.jpg'
 import church from '../.././Images/Gallery/church.jpg'
 import col from '../.././Images/Gallery/col.jpg'
 import columbia from '../.././Images/Gallery/columbia.jpg'
 import damnationcreek_ocean from '../.././Images/Gallery/damnationcreek_ocean.jpg'
+import sardiniasunset from '../.././Images/Gallery/SardiniaSunset.jpg'
 import damnationcreek from '../.././Images/Gallery/damnationcreek.jpg'
 import flor from '../.././Images/Gallery/flor.jpg'
 import finny from '../.././Images/Gallery/finny.jpg'
@@ -31,7 +32,7 @@ import mount_hood from '../.././Images/Gallery/mount_hood.jpg'
 import oeschinenseeClouds from '../.././Images/Gallery/oeschinenseeClouds.jpg'
 import oeschinenseeSide from '../.././Images/Gallery/oeschinenseeSide.jpg'
 import or_coast from '../.././Images/Gallery/or_coast.jpg'
-import or_coast_beach from '../.././Images/Gallery/or_coast_beach.jpg'
+import beach_camp from '../.././Images/Gallery/beach_camp.jpg'
 import pl_mom_dad from '../.././Images/Gallery/pl_mom_dad.jpg'
 import pl_storm from '../.././Images/Gallery/pl_storm.jpg'
 import priestlake_point from '../.././Images/Gallery/priestlake_point.jpg'
@@ -39,7 +40,7 @@ import portland_roses from '../.././Images/Gallery/portland_roses.jpg'
 import rome_bridge from '../.././Images/Gallery/rome_bridge.jpg'
 import rome_street from '../.././Images/Gallery/rome_street.jpg'
 import sardinia from '../.././Images/Gallery/sardinia.jpg'
-import sardiniasunset from '../.././Images/Gallery/SardiniaSunset.jpg'
+import or_coast_beach from '../.././Images/Gallery/or_coast_beach.jpg'
 import super_bloom from '../.././Images/Gallery/super_bloom.jpg'
 import thor from '../.././Images/Gallery/thor.jpg'
 import thunderstormcalm from '../.././Images/Gallery/thunderstormcalm.jpg'
@@ -49,24 +50,31 @@ class PhotographyContent extends React.Component {
   constructor(props) {
     super(props);
 
+
+    var numCols
+    if (window.innerWidth > 2000) {
+      numCols = 3
+    } else if (window.innerWidth > 1200) {
+      numCols = 2
+    } else {
+      numCols = 1
+    }
+
+
     this.state = {
       images: [
-        [aboveguitar, "Guitar Lake, High Sierra Trail"],
-        [alpsRolling, "Above Wengen, Switzerland"],
-        [arno, "Florence, Italy"],
-        [beach_camp, "Oregon Coast"],
+        [mount_hood, "Mount Hood"],
         [ca_coast, "Highway 101, Northern California"],
+        [arno, "Florence, Italy"],
         [carlsbad_sunset, "Near Carlsbad, San Diego"],
-        [church, "Mount Rigi, Switzerland"],
         [whitneysunrise, "Mount Whitney, High Sierra Trail"],
-        [col, "Colosseum, Italy"],
+        [finny, "Finny"],
         [columbia, "Columbia River Gorge"],
         [damnationcreek_ocean, "Waves, Northern California"],
         [rome_street, "Rome, Italy"],
         [damnationcreek, "Damnation Creek, Northern California"],
         [oeschinenseeSide, "Oeschinensee, Switzerland"],
         [flor, "Plaza Michelangelo, Florence"],
-        [finny, "Finny"],
         [fred, "Fredericksburg, VA"],
         [priestlake_point, "Priest Lake, Idaho"],
         [kleinescheidegg, "Kleinescheidegg, Switzerland"],
@@ -74,10 +82,14 @@ class PhotographyContent extends React.Component {
         [lake_lucerne, "Lake Lucerne, Switzerland"],
         [landsendrocks, "Lands End, San Francisco"],
         [lincolncity, "Lincoln City, Oregon"],
+        [aboveguitar, "Guitar Lake, High Sierra Trail"],
+        [beach_camp, "Oregon Coast"],
+        [alpsRolling, "Above Wengen, Switzerland"],
+        [sardiniasunset, "Alghero, Italy"],
         [lugano, "Lugano, Switzerland"],
         [mountrigi, "Mount Rigi, Switzerland"],
         [mount_constitution, "Mount Constitution"],
-        [mount_hood, "Mount Hood"],
+        [church, "Mount Rigi, Switzerland"],
         [oeschinenseeClouds, "Oeschinensee, Switzerland"],
         [or_coast, "Oregon Coast Sunset"],
         [or_coast_beach, "Otter Rock"],
@@ -85,93 +97,70 @@ class PhotographyContent extends React.Component {
         [portland_roses, "Portland Rose Garden"],
         [lugano_swim, "Lugano, Switzerland"],
         [rome_bridge, "Rome, Italy"],
+        [col, "Colosseum, Italy"],
         [sardinia, "Sardinia, Italy"],
-        [sardiniasunset, "Alghero, Italy"],
         [super_bloom, "Desert, Southern California"],
         [thor, "Train, Switzerland"],
         [thunderstormcalm, "After the storm, High Sierra Trail"],
         [geneva, "Geneva, Switzerland"]
       ],
-      img_content: []
+      numCols: numCols
     }
 
-    this.shuffleArray = this.shuffleArray.bind(this)
     this.returnImageArray = this.returnImageArray.bind(this)
+    this.returnImageMasonry = this.returnImageMasonry.bind(this)
+    this.findNumCols = this.findNumCols.bind(this)
+    this.setNumCols = this.setNumCols.bind(this)
   }
 
   componentDidMount() {
-    this.setState({
-      images: this.shuffleArray(this.state.images)
-    })
-
-    this.returnImageArray()
-    window.addEventListener("resize", this.returnImageArray.bind(this));
+    window.addEventListener("resize", this.setNumCols.bind(this));
   }
-
 
   componentWillUnmount() {
-    window.removeEventListener("resize", this.returnImageArray.bind(this));
+    window.removeEventListener("resize", this.setNumCols.bind(this));
   }
 
-  shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+  findNumCols() {
+    if (window.innerWidth > 2000) {
+      return 3
+    } else if (window.innerWidth > 1200) {
+      return 2
+    } else {
+      return 1
     }
-    return array
+  }
+
+  setNumCols() {
+    this.setState({
+      numCols: this.findNumCols()
+    })
   }
 
   returnImageArray() {
     let ret = []
-    {
-      this.state.images.map((value, index) => {
-        if (window.innerWidth > 768) {
-          if (index % 2 == 0 && this.state.images.length >= index + 1) {
-            let img_1 = this.state.images[index][0]
-            let caption_1 = this.state.images[index][1]
-            let img_2 = this.state.images[index + 1][0]
-            let caption_2 = this.state.images[index + 1][1]
-            ret.push(
-              <div className="photography-container">
-                <div className="photo-container">
-                  <a href={img_1} target="_blank">
-                    <img src={img_1} className="photography-image" />
-                  </a>
-                  <div className="body1 photo-caption">{caption_1}</div>
-                </div>
+    this.state.images.map((value, index) => {
+      let img = this.state.images[index][0]
+      ret.push(
+        <img src={img} className="photography-image" />
+      )
+    })
 
-                <div className="photo-container">
-                  <a href={img_2} target="_blank">
-                    <img src={img_2} className="photography-image" />
-                  </a>
-                  <div className="body1 photo-caption">{caption_2}</div>
-                </div>
-              </div>
-            )
-          }
-        } else {
-          let img_1 = this.state.images[index][0]
-          let caption_1 = this.state.images[index][1]
-          ret.push(
-            <div className="photography-container">
-              <div className="photo-container">
-                <a href={img_1} target="_blank">
-                  <img src={img_1} className="photography-image" />
-                </a>
-                <div className="body1 photo-caption">{caption_1}</div>
-              </div>
-            </div>
-          )
-        }
-      })
-    }
-    this.setState({ img_content: ret })
+    return ret
+  }
+
+  returnImageMasonry(numCols) {
+    return <ImageMasonry
+      children={this.returnImageArray()}
+      numCols={numCols}
+    ></ImageMasonry>
   }
 
   render() {
+    let images = this.returnImageMasonry(this.state.numCols)
     return (
       <div className="photography-wrapper">
-        {this.state.img_content}
+        {images}
       </div>
     )
   }
