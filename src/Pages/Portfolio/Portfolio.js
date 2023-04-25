01e1c780adabcad9193402833678e7f121126d81
Matthew Galvin <mgalvin@ds.dfci.harvard.edu>
2023-04-25 07:36:28 -0400

    Style tweaks

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -23,7 +23,7 @@ class Portfolio extends React.Component {
       <div className="page-wrapper">
         <Nav />
         <div className="content">
-          <div className="portfolio-content-container">
+          <div className="portfolio-content-container portfolio-first">
             <p className="body1 port-des general-text-container">
               I have worked on a number of web development and engineering projects in my career. Below you will find a few visual examples of web applications I have authored. If you would like to discuss non-visual or other proprietary work such as API, pipeline, or database development as described in my <a class="nav-link unstyled-link" href="/MatthewGalvin_Resume_Public.pdf" target="_blank" rel="noopener noreferrer">resume</a>, please send me an <a className="unstyled-link" href="mailto:matthewrgalvin@gmail.com">email</a>.
             </p>
